27fad41f4ccb00ec60584b1b916b31ff41c5fd13
Iman Taouil <itaouil@c3r1p4.42nice.fr>
2024-10-22 03:26:26 +0200

    first part done

--- describe.py
+++ describe.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+import math
+import sys
+import os
 
 def load(path: str):
     ret = None
@@ -9,11 +12,11 @@ def load(path: str):
             raise ValueError("Path must be a string")
         if (path.endswith(".csv") is False):
             raise ValueError("Path must be a csv file")
+        if (os.path.exists(path) is False):
+            raise ValueError("File not found")
         ret = pd.read_csv(path)
-        print("Loading dataset:", ret.shape)
     except Exception as e:
         print(e)
-
     return ret
 
 def remove_columns(df: pd.DataFrame, columns: list):
@@ -44,6 +47,7 @@ def count_rows_in_col(df: pd.DataFrame, col:str):
             raise ValueError("col must be a string")
         if (col not in df.columns):
             raise ValueError("Column not found")
+        
         ret = df[col].shape[0]
     except Exception as e:
         print(e)
@@ -67,7 +71,7 @@ def sum_of_col(df: pd.DataFrame, col:str):
         print("Error in sum_of_col:", e)
     return sum
 
-def mean_of_col(df: pd.DataFrame, col:str):
+def mean_of_col(df: pd.DataFrame, col:str, nb_of_cols: int):
     mean = 0
 
     try:
@@ -79,7 +83,6 @@ def mean_of_col(df: pd.DataFrame, col:str):
             raise ValueError("Column not found")
 
         total = sum_of_col(df, col)
-        nb_of_cols = count_rows_in_col(df, col)
         mean = total / nb_of_cols
     except Exception as e:
         print("Error in mean_of_col:", e)
@@ -123,7 +126,7 @@ def max_of_col(df: pd.DataFrame, col:str):
         print("Error in max_of_col:", e)
     return max
 
-def first_quartile_of_col(df: pd.DataFrame, col: str):
+def first_quartile_of_col(df: pd.DataFrame, col: str, length: int):
     ret = 0
     try:
         if (isinstance(df, pd.DataFrame) is False):
@@ -132,18 +135,16 @@ def first_quartile_of_col(df: pd.DataFrame, col: str):
             raise ValueError("col must be a string")
         if (col not in df.columns):
             raise ValueError("Column not found")
-        
         df[col] = df[col].sort_values().values
-        length = count_rows_in_col(df, col)
         if (length % 2 == 0):
             ret = (df[col][length // 4] + df[col][length // 4 + 1]) / 2
         else:
             ret = df[col][length // 4]
     except Exception as e:
-        print("Error in first_quartile_of_col:", e)
+        print("Error in first_quartile_of_col:", type(e).__name__)
     return ret
 
-def third_quartile_of_col(df: pd.DataFrame, col: str):
+def third_quartile_of_col(df: pd.DataFrame, col: str, length: int):
     ret = 0
     try:
         if (isinstance(df, pd.DataFrame) is False):
@@ -154,7 +155,7 @@ def third_quartile_of_col(df: pd.DataFrame, col: str):
             raise ValueError("Column not found")
         
         df[col] = df[col].sort_values().values
-        length = count_rows_in_col(df, col)
+        # length = count_rows_in_col(df, col)
         if (length % 2 == 0):
             ret = (df[col][3 * length // 4] + df[col][3 * length // 4 + 1]) / 2
         else:
@@ -163,7 +164,7 @@ def third_quartile_of_col(df: pd.DataFrame, col: str):
         print("Error in first_quartile_of_col:", e)
     return ret
 
-def median_of_col(df: pd.DataFrame, col: str):
+def median_of_col(df: pd.DataFrame, col: str, length: int):
     ret = 0
     try:
         if (isinstance(df, pd.DataFrame) is False):
@@ -174,7 +175,6 @@ def median_of_col(df: pd.DataFrame, col: str):
             raise ValueError("Column not found")
         
         df[col] = df[col].sort_values().values
-        length = count_rows_in_col(df, col)
         if (length % 2 == 0):
             ret = (df[col][length // 2] + df[col][length // 2 + 1]) / 2
         else:
@@ -183,11 +183,61 @@ def median_of_col(df: pd.DataFrame, col: str):
         print("Error in median_of_col:", e)
     return ret
 
+def standard_deviation_of_col(df: pd.DataFrame, col: str, length: int):
+    ret = 0
+    try:
+        if (isinstance(df, pd.DataFrame) is False):
+            raise ValueError("df must be a DataFrame")
+        if (isinstance(col, str) is False):
+            raise ValueError("col must be a string")
+        if (col not in df.columns):
+            raise ValueError("Column not found")
+        mean = mean_of_col(df, col, length)
+        sum_of_squares = 0
+
+        for row in df[col]:
+            if (pd.notna(row)):
+                sum_of_squares += (row - mean) ** 2
+        var = sum_of_squares / length
+        ret = math.sqrt(var) 
+    except Exception as e:
+        print("Error in standard_deviation_of_col:", e)
+    return ret
+
+def ft_describe(df: pd.DataFrame):
+    ret = None
+    try:
+        if (isinstance(df, pd.DataFrame) is False):
+            raise ValueError("df must be a DataFrame")
+        
+        for col in df.columns:
+            length = count_rows_in_col(df, col)
+            mean = mean_of_col(df, col, length)
+            std = standard_deviation_of_col(df, col, length)
+            min = min_of_col(df, col)
+            first_quartile = first_quartile_of_col(df, col, length)
+            median = median_of_col(df, col, length)
+            third_quartile = third_quartile_of_col(df, col, length)
+            max = max_of_col(df, col)
+            rows = [length, mean, std, min, first_quartile, median, third_quartile, max]
+            if (ret is None):
+                ret = pd.DataFrame({col: rows})
+                ret.index = ["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"]
+            else:
+                ret.insert(len(ret.columns), col, rows)
+    except Exception as e:
+        print("Error in ft_describe:", e)
+    return ret
+
 if __name__ == "__main__":
-    df = load("./datasets/dataset_test.csv")
+    if len(sys.argv) < 2:
+        print("Usage: python describe.py <path>")
+        sys.exit(1)
+    df = load(sys.argv[1])
+    if (df is None):
+        sys.exit(1)
     cleaned_df = remove_columns(df, ["Index", "Hogwarts House", "First Name", "Last Name", "Birthday", "Best Hand"])
-    print("sum:", sum_of_col(cleaned_df, "Potions"))
-    print("total:", count_rows_in_col(cleaned_df, "Potions"))
-    print("mean:", mean_of_col(cleaned_df, "Potions"))
-    print("min:", min_of_col(cleaned_df, "Potions"))
-    print("max:", max_of_col(cleaned_df, "Potions"))
+    cleaned_df = cleaned_df.dropna(ignore_index=True)
+
+    print(ft_describe(cleaned_df))
+
