87d6a61cadc1bc0c268e649614ac0db77cf605e6
ImanSafae <iman-104@hotmail.fr>
2024-10-24 18:59:06 +0200

    closer to histo

--- histogram.py
+++ histogram.py
@@ -13,6 +13,7 @@ def extract_house(df: pd.DataFrame, house: str):
             raise ValueError("Second column must be 'Hogwarts House'")
         
         ret = df[df['Hogwarts House'] == house].reset_index(drop=True)
+        ret = ret.drop(columns=['Hogwarts House'])
     except Exception as e:
         print(e)
     return ret
@@ -48,45 +49,15 @@ if __name__ == "__main__":
                 # Extraire la colonne + Hogwarts House dans un nouveau dataframe
                 new_df = cleaned_df[[col, 'Hogwarts House']]
                 
-                # Extraire les données pour chaque maison
-                ravenclaw_data = ft_describe(extract_house(new_df, 'Ravenclaw')[[col]].copy()).loc[['Mean', 'Std']]
-                gryffindor_data = ft_describe(extract_house(new_df, 'Gryffindor')[[col]].copy()).loc[['Mean', 'Std']]
-                hufflepuff_data = ft_describe(extract_house(new_df, 'Hufflepuff')[[col]].copy()).loc[['Mean', 'Std']]
-                slytherin_data = ft_describe(extract_house(new_df, 'Slytherin')[[col]].copy()).loc[['Mean', 'Std']]
-                
-                # Convertir les données en scalaires
-                bars_mean = [
-                    float(ravenclaw_data.loc['Mean'].values[0]), 
-                    float(gryffindor_data.loc['Mean'].values[0]), 
-                    float(hufflepuff_data.loc['Mean'].values[0]), 
-                    float(slytherin_data.loc['Mean'].values[0])
-                ]
-                bars_std = [
-                    float(ravenclaw_data.loc['Std'].values[0]), 
-                    float(gryffindor_data.loc['Std'].values[0]), 
-                    float(hufflepuff_data.loc['Std'].values[0]), 
-                    float(slytherin_data.loc['Std'].values[0])
-                ]
+                ravenclaw_values = (extract_house(new_df, 'Ravenclaw')).values.tolist()
+                gryffindor_values = (extract_house(new_df, 'Gryffindor')).values.tolist()
+                hufflepuff_values = (extract_house(new_df, 'Hufflepuff')).values.tolist()
+                slytherin_values = (extract_house(new_df, 'Slytherin')).values.tolist()
 
-                # Créer le graphique
-                fig, ax = plt.subplots()
-                index = np.arange(4)  # Pour 4 maisons
-                bar_width = 0.35
-
-                # Dessiner les barres
-                ax.bar(index, bars_mean, bar_width, label='Mean', color='blue')
-                ax.bar(index + bar_width, bars_std, bar_width, label='Std', color='orange')
-
-                # Personnalisation du graphique
-                ax.set_xlabel('Hogwarts House')
-                ax.set_ylabel('Scores')
-                ax.set_title(f'{col} Statistics by House')
-                ax.set_xticks(index + bar_width / 2)
-                ax.set_xticklabels(['Ravenclaw', 'Gryffindor', 'Hufflepuff', 'Slytherin'])
-                ax.legend()
-
-                # Afficher le graphique
-                plt.show()
+                print("ravenclaw values:\n", ravenclaw_values)
+                print("gryffindor values:\n", gryffindor_values)
+                print("hufflepuff values:\n", hufflepuff_values)
+                print("slytherin values:\n", slytherin_values)
 
 
 
